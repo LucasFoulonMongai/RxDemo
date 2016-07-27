01d7e6fdc9dbe38f038cb39d5018b5853bd33b96
Lucas Foulon-Mongaï <mogwai@pc13.home>
2016-07-27 20:15:10 +0200

    Rx error and naming

--- app/src/main/java/com/lfm/rxdemo/rx/Rx.java
+++ app/src/main/java/com/lfm/rxdemo/rx/Rx.java
@@ -7,9 +7,11 @@ import rx.functions.Action1;
  * Created by Lucas FOULON-MONGAÏ, github.com/LucasFoulonMongai on 28/02/2016.
  */
 public class Rx {
-    public static final Action1<Throwable> NO_ERROR = throwable -> {
-        throwable.printStackTrace();
+    public static final Action1<Throwable> EMPTY_ERROR = throwable -> {
     };
+
+    public static final Action1<Throwable> PRINT_ERROR = Throwable::printStackTrace;
+
     public static final Action0 EMPTY = () -> {
     };
 
